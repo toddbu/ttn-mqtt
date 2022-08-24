a3079bf91bfce9a1a724e725f82a3fbda0dd602e
Todd Buiten <todd@buiten.com>
2022-08-23 18:09:40 -0700

    Created

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const mqtt = require('mqtt')
+const client = mqtt.connect('mqtts://nam1.cloud.thethings.network:8883', {
+  username: 'toddbu-temperature@ttn',
+  password: 'NNSXS.OGAANBBTJQZCAG7OL6RVKB4LGXMY4EXATQUNMUQ.7YYUJILVXUKXISLEUWSDROPA45L52HQK3T4VJSUCTB53EJGOGDDA'
+})
+
+client.on('connect', () => {
+  client.subscribe('v3/toddbu-temperature@ttn/devices/pico-98-76-b6-12-04-38/up', (err) => {
+    if (err) {
+      console.log(err)
+
+      return
+    }
+
+    //$ client.publish('presence', 'Hello mqtt')
+  })
+})
+
+client.on('message', (topic, message) => {
+  // message is Buffer
+  console.log(message.toString())
+  console.log()
+  //$ client.end()
+})
