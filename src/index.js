d751cd8cf30d4fa8da07a7706968a265e0cb9907
Todd Buiten <todd@buiten.com>
2023-02-22 21:13:35 +0000

    Write values to Cassandra

--- src/index.js
+++ src/index.js
@@ -1,26 +1,44 @@
 const mqtt = require('mqtt')
-const client = mqtt.connect('mqtts://nam1.cloud.thethings.network:8883', {
+const cassandra = require('cassandra-driver');
+
+const cassandraClient = new cassandra.Client({
+  contactPoints: ['10.0.0.80'],
+  localDataCenter: 'datacenter1',
+  keyspace: 'temperature'
+});
+
+const mqttClient = mqtt.connect('mqtts://nam1.cloud.thethings.network:8883', {
   username: 'toddbu-temperature@ttn',
   password: 'NNSXS.OGAANBBTJQZCAG7OL6RVKB4LGXMY4EXATQUNMUQ.7YYUJILVXUKXISLEUWSDROPA45L52HQK3T4VJSUCTB53EJGOGDDA'
 })
 
-client.on('connect', () => {
-  client.subscribe('v3/toddbu-temperature@ttn/devices/eui-9876b60000120438/up', (err) => {
+mqttClient.on('connect', () => {
+  mqttClient.subscribe('v3/toddbu-temperature@ttn/devices/eui-9876b60000120438/up', (err) => {
     if (err) {
       console.log(err)
 
       return
     }
 
-    //$ client.publish('presence', 'Hello mqtt')
+    //$ mqttClient.publish('presence', 'Hello mqtt')
   })
 })
 
-client.on('message', (topic, message) => {
+mqttClient.on('message', (topic, message) => {
   // message is Buffer
-  //$ console.log(new Date())
   const uplinkMessage = JSON.parse(message.toString()).uplink_message
   console.log(`${uplinkMessage.received_at} - ${uplinkMessage.decoded_payload.temp}F`)
-  //$ console.log()
-  //$ client.end()
+
+
+  const query = 'INSERT INTO temperature.temperature (station_id, received_at, temperature_in_f) VALUES(?, ?, ?)'
+  const params = [
+    1,
+    uplinkMessage.received_at,
+    uplinkMessage.decoded_payload.temp
+  ]
+
+  //$ How to make async?
+  cassandraClient.execute(query, params, { prepare: true });
+
+  //$ mqttClient.end()
 })
