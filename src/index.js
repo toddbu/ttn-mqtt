2099ddadfed16d484e09400aaa63bfc907d22c0f
Todd Buiten <todd@buiten.com>
2023-04-09 01:29:13 +0000

    Refactor return_payload

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ const request = require('request')
 const cassandra = require('cassandra-driver')
 const config = require('./config')(process.env.ENV)
 
-const DEFAULT_RETURN_PAYLOAD = 'AA==' // One byte binary 0 as a default
-
 const cassandraClient = new cassandra.Client({
   contactPoints: ['10.0.0.80'],
   localDataCenter: 'datacenter1',
@@ -33,46 +31,46 @@ function formatDateComponent(value) {
   return value.toString().padStart(2, 0)
 }
 
-function handle_time_sync(message_type, message_bytes, serverDate) {
-  let return_payload = DEFAULT_RETURN_PAYLOAD
+function handle_time_sync(message_type, content_bytes, serverDate) {
+  let return_payload = null
 
   switch (message_type) {
     case 0:
-      const payloadBytes = Buffer.from(message_bytes)
+      const payloadBytes = Buffer.from(content_bytes)
 
       console.log(`datetime request`)
 
       // Check to see if we get a NOP. If we did then send back a packet with no
       // adjustments since we're just trying to force a download
-      if (message_bytes[4] +
-          message_bytes[5] +
-          message_bytes[6] +
-          message_bytes[7] +
-          message_bytes[8] +
-          message_bytes[9] +
-          message_bytes[10] === 0) {
-        for (let i = 4; i < 11; i++) {
+      if (content_bytes[0] +
+          content_bytes[1] +
+          content_bytes[2] +
+          content_bytes[3] +
+          content_bytes[4] +
+          content_bytes[5] +
+          content_bytes[6] === 0) {
+        for (let i = 0; i < 7; i++) {
           payloadBytes[i] = 128
         }
-        return_payload = payloadBytes.toString('base64')
+        return_payload = payloadBytes
         console.log('NOP', return_payload, payloadBytes)
-        break;
+        break
       }
 
-      const clientDate = new Date(`${formatDateComponent(message_bytes[4])}${formatDateComponent(message_bytes[5])}-${formatDateComponent(message_bytes[6])}-${formatDateComponent(message_bytes[7])} ${formatDateComponent(message_bytes[8])}:${formatDateComponent(message_bytes[9])}:${formatDateComponent(message_bytes[10])}Z`)
+      const clientDate = new Date(`${formatDateComponent(content_bytes[0])}${formatDateComponent(content_bytes[1])}-${formatDateComponent(content_bytes[2])}-${formatDateComponent(content_bytes[3])} ${formatDateComponent(content_bytes[4])}:${formatDateComponent(content_bytes[5])}:${formatDateComponent(content_bytes[6])}Z`)
 
       // The Pico is pretty limited in datetime processing capabillites,
       // so we'll calcullate the RTC offsets here. Also, because we can
       // only send unsigned bytes then we'll add 128 to the calculated
       // value so that we can have negative numbers
-      payloadBytes[4] = parseInt((serverDate.getFullYear() - clientDate.getFullYear()) / 100) + 128
-      payloadBytes[5] = parseInt((serverDate.getFullYear() - clientDate.getFullYear()) % 100) + 128
-      payloadBytes[6] = serverDate.getMonth() - clientDate.getMonth() + 128
-      payloadBytes[7] = serverDate.getDate() - clientDate.getDate() + 128
-      payloadBytes[8] = serverDate.getHours() - clientDate.getHours() + 128
-      payloadBytes[9] = serverDate.getMinutes() - clientDate.getMinutes() + 128
-      payloadBytes[10] = serverDate.getSeconds() - clientDate.getSeconds() + 128
-      return_payload = payloadBytes.toString('base64')
+      payloadBytes[0] = parseInt((serverDate.getFullYear() - clientDate.getFullYear()) / 100) + 128
+      payloadBytes[1] = parseInt((serverDate.getFullYear() - clientDate.getFullYear()) % 100) + 128
+      payloadBytes[2] = serverDate.getMonth() - clientDate.getMonth() + 128
+      payloadBytes[3] = serverDate.getDate() - clientDate.getDate() + 128
+      payloadBytes[4] = serverDate.getHours() - clientDate.getHours() + 128
+      payloadBytes[5] = serverDate.getMinutes() - clientDate.getMinutes() + 128
+      payloadBytes[6] = serverDate.getSeconds() - clientDate.getSeconds() + 128
+      return_payload = payloadBytes
       console.log(clientDate, return_payload, payloadBytes)
       break
 
@@ -84,8 +82,8 @@ function handle_time_sync(message_type, message_bytes, serverDate) {
   return return_payload
 }
 
-async function handle_app_update(message_type, message_bytes, clientMessageDate, decoded_payload) {
-  let return_payload = DEFAULT_RETURN_PAYLOAD
+async function handle_app_update(message_type, content_bytes, clientMessageDate, decoded_payload) {
+  let return_payload = null
 
   switch (message_type) {
     case 1:
@@ -98,7 +96,7 @@ async function handle_app_update(message_type, message_bytes, clientMessageDate,
       ]
 
       await cassandraClient.execute(query, params, { prepare: true });
-      return_payload = Buffer.from([message_bytes[0], message_bytes[1], message_bytes[2], message_bytes[3]]).toString('base64')
+      return_payload = Buffer.from([content_bytes[0], content_bytes[1], content_bytes[2], content_bytes[3]])
       break
 
     default:
@@ -123,11 +121,18 @@ mqttClient.on('message', async (topic, message) => {
   const decoded_payload = uplinkMessage.decoded_payload
 
   process.stdout.write(`${uplinkMessage.received_at} - `)
-  const message_bytes = uplinkMessage.decoded_payload.bytes
-  console.log(message_bytes)
+  const all_bytes = decoded_payload.bytes
+  console.log(all_bytes)
+
+  const header_bytes = Buffer.from(all_bytes.slice(0, 4))
+  const content_bytes = all_bytes.slice(4)
+  process.stdout.write('raw bytes: ')
+  for (i = 0; i < 4 + decoded_payload.contentLength; i++) {
+    process.stdout.write(`${all_bytes[i]} `)
+  }
+  process.stdout.write('\n')
 
   // Figure out the date based on the timestamp
-  //$ const serverDate = new Date('2023-02-28 04:00:33Z')
   const serverDate = new Date()
   const serverDow = serverDate.getUTCDay()
   const serverBeginningOfDay = new Date(serverDate).setUTCHours(0, 0, 0, 0)
@@ -143,29 +148,24 @@ mqttClient.on('message', async (topic, message) => {
   const clientMessageDate = new Date(serverBeginningOfDay.valueOf() + (offsetDays * 86400000) + (clientSecondsPastMidnight * 1000))
   console.log(`clientMessageDate = ${clientMessageDate}`)
 
-  process.stdout.write('raw bytes: ')
-  for (i = 0; i < 4 + decoded_payload.contentLength; i++) {
-    process.stdout.write(`${message_bytes[i]} `)
-  }
-  process.stdout.write('\n')
-
   switch (fPort) {
     case 1:
-      return_payload = await handle_app_update(decoded_payload.type, message_bytes, clientMessageDate, decoded_payload)
+      return_payload = await handle_app_update(decoded_payload.type, content_bytes, clientMessageDate, decoded_payload)
       break
 
     case 222:
-      return_payload = handle_time_sync(decoded_payload.type, message_bytes, serverDate)
+      return_payload = handle_time_sync(decoded_payload.type, content_bytes, serverDate)
       break
 
     default:
       console.log(`Unknown port: ${fPort}`)
-      return_payload = DEFAULT_RETURN_PAYLOAD
+      return_payload = payload
       break
   }
 
   //$ How to make this async call wait, or do we really care???
-  console.log(`Return payload = ${return_payload}`)
+  console.log(`Return payload = ${[...return_payload]}`)
+
   request.post(`https://nam1.cloud.thethings.network/api/v3/as/applications/${config.applicationName}/devices/${config.deviceEui}/down/push`, {
     headers: {
       Authorization: `Bearer ${config.password}`,
@@ -176,7 +176,7 @@ mqttClient.on('message', async (topic, message) => {
     body: {
       downlinks: [
         {
-          frm_payload: return_payload,
+          frm_payload: (return_payload ? Buffer.concat([header_bytes, return_payload]) : header_bytes).toString('base64'),
           f_port: fPort,
           priority: 'NORMAL'
         }
